cc0c96187937b4e58832028bd2fe30998582ce5a
Said Berk <csaidberk@gmail.com>
2023-05-07 01:03:24 +0300

    Product page v1 Design completed + pie chart added.

---
+++ lib/screens/add_new_product.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddNewProductsPage extends StatelessWidget {
+  const AddNewProductsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: const Color(0xFF2DBD3A));
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,7 @@ import 'package:envanter_kontrol/utils/colors.dart';
 import 'package:envanter_kontrol/utils/text_styles.dart';
 import 'package:envanter_kontrol/widgets/custom_fab.dart';
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
@@ -13,6 +14,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Map<String, double> dataMap = {
+    "Gömlek": 23,
+    "Tişört": 67,
+    "Eşofman Altı": 97,
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,23 +32,77 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Row(
-                children: [
-                  Expanded(
-                      flex: 3,
-                      child: Text(
-                        "KATEGORİ ADI",
-                        style: ProjectTextStyle.redMediumStrong,
-                        textAlign: TextAlign.center,
-                      )),
-                  Expanded(flex: 1, child: searchTextField()),
-                ],
-              )
+              Expanded(
+                flex: 1,
+                child: headerSection(),
+              ),
+              Expanded(
+                flex: 4,
+                child: productsListView(),
+              ),
+              Expanded(flex: 3, child: pieChart(dataMap: dataMap))
             ],
           ),
         ),
       ),
       floatingActionButton: const CustomFab(),
+    );
+  }
+
+  PieChart pieChart({required Map<String, double> dataMap}) => PieChart(
+        dataMap: dataMap,
+        animationDuration: const Duration(seconds: 2),
+      );
+
+  Row headerSection() {
+    return Row(
+      children: [
+        Expanded(
+            flex: 3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "KATEGORİ ADI",
+                  style: ProjectTextStyle.redMediumStrong,
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  "Toplam Stok: 345",
+                  style: ProjectTextStyle.redMedium,
+                )
+              ],
+            )),
+        Expanded(flex: 1, child: searchTextField()),
+      ],
+    );
+  }
+
+  ListView productsListView() {
+    return ListView(
+      children: [
+        ListTile(
+          leading: Image.asset("assets/images/tshirt.png"),
+          title: const Text("Siyah Tişört"),
+          subtitle: const Text("Stok: 67"),
+          trailing: const Icon(Icons.arrow_forward_ios),
+          onTap: () {},
+        ),
+        ListTile(
+          leading: Image.asset("assets/images/shirt.png"),
+          title: const Text("Siyah Gömlek"),
+          subtitle: const Text("Stok: 23"),
+          trailing: const Icon(Icons.arrow_forward_ios),
+          onTap: () {},
+        ),
+        ListTile(
+          leading: Image.asset("assets/images/pants.png"),
+          title: const Text("Siyah Eşofman Altı"),
+          subtitle: const Text("Stok: 97"),
+          trailing: const Icon(Icons.arrow_forward_ios),
+          onTap: () {},
+        ),
+      ],
     );
   }
 
@@ -51,7 +112,7 @@ class _HomePageState extends State<HomePage> {
       textAlignVertical: TextAlignVertical.center,
       style: ProjectTextStyle.redSmallStrong,
       decoration: InputDecoration(
-          prefixIcon: Icon(Icons.search),
+          prefixIcon: const Icon(Icons.search),
           suffix: IconButton(
               onPressed: () {},
               icon: Icon(
@@ -63,7 +124,7 @@ class _HomePageState extends State<HomePage> {
               borderSide:
                   BorderSide(width: 5, color: ProjectColors.projectRed)),
           enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(24)),
+              borderRadius: const BorderRadius.all(Radius.circular(24)),
               borderSide:
                   BorderSide(width: 5, color: ProjectColors.projectRed))),
     );
